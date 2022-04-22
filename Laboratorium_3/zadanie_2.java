33595e091b448ac92cf0eb510f7682a4235d5c6c
s24280 <93827191+s24280@users.noreply.github.com>
2022-04-23 00:41:10 +0200

    Add files via upload

---
+++ Laboratorium_3/zadanie_2.java
@@ -0,0 +1,89 @@
+class Author {
+    String name;
+    String email;
+    char gender;
+
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Author[" +
+                "name=" + getName() +
+                ",email=" + getEmail() +
+                ",gender=" + getGender() +
+                ']';
+    }
+}
+class Book {
+    String name;
+    Author author;
+    double price;
+    int qty = 0;
+    public Book(String name, double price, Author author) {
+    this.name=name;
+    this.author=author;
+    this.price=price;
+    }
+
+    public Book(String name, double price, Author author, int qty) {
+        this.name=name;
+        this.author=author;
+        this.price=price;
+        this.qty=qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        return "Book[name=" + getName() +
+                ",author=" +
+                author.toString() +
+                ",price=" + getPrice() +
+                ",qty=" + getQty() +
+                ']';
+    }
+}
+
